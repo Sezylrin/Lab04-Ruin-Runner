ccce9a1fa3f9c0efd18b40361a57addaf18b3e4c
Xero8252 <39457241+Xero-oce@users.noreply.github.com>
2023-03-18 20:01:44 +1100

    Level 1 Maze

---
+++ Assets/Scripts/ExitDoor.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExitDoor : MonoBehaviour
+{
+
+    private BoxCollider2D exitTrigger;
+    // Start is called before the first frame update
+    void Start()
+    {
+        exitTrigger = this.gameObject.GetComponent<BoxCollider2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        //if (keys == maxKeys)
+        //{
+            Loader.Load(Scene.Victory);
+        //}
+    }
+}
