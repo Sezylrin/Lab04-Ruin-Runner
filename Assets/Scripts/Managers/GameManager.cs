f751ee517cdfc49e6f449dfb730a8845af9c3956
Edoardo <edoardomenon1997@gmail.com>
2023-03-19 16:02:03 +1100

    Add coins that spawn keys

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -6,9 +6,16 @@ public class GameManager : MonoBehaviour
 {
     public static GameManager Instance { get; private set; }
 
+    [SerializeField] private int coinAmountToSpawnKey;
+    [SerializeField] private GameObject key;
+
     public int keysCollected { get; private set; }
     public int lives { get; private set; }
     public int score { get; private set; }
+    private int coinsCollected = 0;
+    private int level = 1;
+    private bool canSpawnKey = true;
+    private Vector2[] keySpawnLocations = new Vector2[] { new Vector2(-10f, 7.55f) };
 
     private void Awake()
     {
@@ -28,6 +35,7 @@ public class GameManager : MonoBehaviour
         keysCollected = 0;
         lives = 0;
         score = 0;
+        coinsCollected = 0;
     }
 
     public void Respawn()
@@ -54,7 +62,27 @@ public class GameManager : MonoBehaviour
         LevelManager.Instance.SetLives(lives.ToString());
     }
 
-    public void AddToScore(int scoreToAdd)
+    public void CollectCoin()
+    {
+        coinsCollected++;
+        AddToScore(50);
+        LevelManager.Instance.SetScore(score.ToString());
+        if (coinsCollected % coinAmountToSpawnKey == 0 && canSpawnKey)
+        {
+            SpawnKey();
+        }
+    }
+
+    private void SpawnKey()
+    {
+        if (keysCollected == level)
+        {
+            canSpawnKey = false;
+        }
+        Instantiate(key, keySpawnLocations[0], Quaternion.identity);
+    }
+
+    private void AddToScore(int scoreToAdd)
     {
         score += scoreToAdd;
         LevelManager.Instance.SetScore(score.ToString());
